01bc6cc27648690c58e0b98ca5c3d8cee9b39aac
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-12 18:59:56 +0200

    Update finish App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,8 +9,7 @@ const Home = () => {
 }
 const News = () => <h1>Aktualności</h1>;
 const Contact = () => <h1>Kontakt do nas</h1>;
-
-
+const ErrorPage = () => <h1>Strona nie istnieje!</h1>
 
 class App extends Component {
   state = {}
@@ -21,16 +20,19 @@ class App extends Component {
           <header>
             <nav>
               <ul>
-                <li><NavLink to="/" exact activeClassName="home_selected" activeStyle={{bacgroundColor: 'gray', letterSpacing: '6px'}}>Start</NavLink></li>
+                <li><NavLink to="/" exact activeClassName="home_selected" activeStyle={{ bacgroundColor: 'gray', letterSpacing: '6px' }}>Start</NavLink></li>
                 <li><NavLink to="/news" activeClassName="news_selected">Aktualności</NavLink></li>
                 <li><NavLink to="/contact" activeClassName="contact_selected">Kontakt</NavLink></li>
               </ul>
             </nav>
           </header>
           <section>
+            <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/news" component={News} />
             <Route path="/contact" component={Contact} />
+            <Route component={ErrorPage} />
+            </Switch>
           </section>
         </div>
       </Router>
